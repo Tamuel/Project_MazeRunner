e43210ae6209987d6536c6ce6d8bfc8916cd837e
우석 <mell03@naver.com>
2015-10-12 00:10:49 +0900

    Defined Data Structure of Data Tables and some test function of file IO

--- Source/Project_MazeRunner/MazeQuizGameMode.cpp
+++ Source/Project_MazeRunner/MazeQuizGameMode.cpp
@@ -3,6 +3,14 @@
 #include "Project_MazeRunner.h"
 #include "MazeQuizGameMode.h"
 
+void AMazeQuizGameMode::TestFileOutput()
+{
+	FString gameContentDir = FPaths::GameContentDir();
+	FString writeDIr = FPaths::Combine(*gameContentDir
+										, TEXT("/Resources/Q.txt"));
+
+	FFileHelper::SaveStringToFile("This is for test", *writeDIr);
+}
 
 
 
--- Source/Project_MazeRunner/MazeQuizGameMode.h
+++ Source/Project_MazeRunner/MazeQuizGameMode.h
@@ -2,18 +2,33 @@
 
 #pragma once
 
+#include "CoreMisc.h"
 #include "GameFramework/GameMode.h"
 #include "MazeQuizGameMode.generated.h"
 
 /**
  * 
  */
+
+/** Structure to store the lookup of Quizes */
+USTRUCT(Blueprintable)
+struct FQuizGameTable : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+		
+	/** Quiz */
+	UPROPERTY(BlueprintReadWrite, Category = "Quiz")
+	FString Quiz;
+
+};
+
 UCLASS()
 class PROJECT_MAZERUNNER_API AMazeQuizGameMode : public AGameMode
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+
+	/* A function which will be use
+		when read quizes from cvs file */
+	UFUNCTION(BlueprintCallable, Category = "Test")
+	void TestFileOutput();
 };
