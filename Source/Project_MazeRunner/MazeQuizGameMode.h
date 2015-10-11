627dbb88c2b418c315c56537f8169465a9fe6443
우석 <mell03@naver.com>
2015-10-11 22:36:52 +0900

    Game Mode 구현을 위한 Game Mode C++클래스 추가.

---
+++ Source/Project_MazeRunner/MazeQuizGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MazeQuizGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_MAZERUNNER_API AMazeQuizGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
